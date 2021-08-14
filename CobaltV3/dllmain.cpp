ab22dc5f5806c2601d3d8f78fd5ab07ef6078c2d
Milxnor <84999745+Milxnor@users.noreply.github.com>
2021-08-14 13:34:10 +0000

    Create dllmain.cpp

---
+++ CobaltV3/dllmain.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+    switch (dwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        Main();
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
